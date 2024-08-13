9a394b7b5f48131afff413ac846ffa54c248a757
danielxf007 <daniel.higuita007@gmail.com>
2024-08-12 19:55:43 -0500

    Created deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+create_bucket() {
+  echo "Deployed Bucket"
+}
+
+create_lambda() {
+  echo "Deployed Lambda"
+}
+
+deploy_lambda_code() {
+    echo "Deployed Lambda Code"
+}
+
+case "$subcommand" in
+    create-bucket) create_bucket "$@"; exit;;
+    create-lambda) create_lambda( "$@"; exit;;
+    deploy-lambda-code) deploy_lambda_code( "$@"; exit;;
+    *) echo "Unimplemented command: $subcommand" >&2; exit 1;;
+esac
