e27092f227d3e9f520c194c11ed8e57ee57ef383
danielxf007 <daniel.higuita007@gmail.com>
2024-08-14 14:47:11 -0500

    Updated create bucket

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,13 @@
 
 create_bucket() {
 
+  STACK_NAME=bucker-deployment
+  DOMAIN_AWS_PROFILE="default"
+  REGION=$(aws configure get region --profile "$DOMAIN_AWS_PROFILE")
+
+  echo $STACK_NAME
+  echo $DOMAIN_AWS_PROFILE
+  echo $REGION
   echo "Deployed Bucket"
 }
 
