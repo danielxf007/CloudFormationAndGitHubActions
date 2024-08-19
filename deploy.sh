a1a1cb0cc48f842bde8e265786e25ae418a2c99f
danielxf007 <daniel.higuita007@gmail.com>
2024-08-19 10:50:35 -0500

    updated deployment

--- deploy.sh
+++ deploy.sh
@@ -4,39 +4,27 @@ DIRNAME=$PWD
 create_bucket() {
 
   STACK_NAME=bucket-deployment
-  if ! $(aws cloudformation describe-stacks --stack-name "$STACK_NAME"); then
-    echo "Stack does not exist ..."
-    echo "Creating Stack ..."
-    echo $(aws cloudformation deploy --stack-name "$STACK_NAME" \
-          --template-file "$DIRNAME"/templates/bucket.yaml \
-          --parameter-overrides \
-            bucketName="$AWS_BUCKET" \
-          --capabilities "CAPABILITY_NAMED_IAM" \
-          --region "$AWS_REGION"
-          )
-    echo "Deployed Bucket"
-  else
-    echo "$STACK_NAME Stack exists"
-  fi
-  
+  echo "Creating Stack ..."
+  echo $(aws cloudformation deploy --stack-name "$STACK_NAME" \
+        --template-file "$DIRNAME"/templates/bucket.yaml \
+        --parameter-overrides \
+          bucketName="$AWS_BUCKET" \
+        --capabilities "CAPABILITY_NAMED_IAM" \
+        --region "$AWS_REGION"
+        )
+  echo "Deployed Bucket"
 }
 
 create_lambda() {
 
   STACK_NAME=lambda-deployment
-  if ! $(aws cloudformation describe-stacks --stack-name "$STACK_NAME"); then
-    echo "Stack does not exist ..."
-    echo "Creating Stack ..."
-    echo $(aws cloudformation deploy --stack-name "$STACK_NAME" \
-          --template-file "$DIRNAME"/templates/lambda.yaml \
-          --parameter-overrides \
-          --capabilities "CAPABILITY_NAMED_IAM" \
-          --region "$AWS_REGION"
-          )
-    echo "Deployed Lambda"
-  else
-    echo "$STACK_NAME Stack exists"
-  fi
+  echo "Creating Stack ..."
+  echo $(aws cloudformation deploy --stack-name "$STACK_NAME" \
+        --template-file "$DIRNAME"/templates/lambda.yaml \
+        --capabilities "CAPABILITY_NAMED_IAM" \
+        --region "$AWS_REGION"
+        )
+  echo "Deployed Lambda"
 }
 
 subcommand=$1
